7132e5faeb544627e29cfb0db5050bef8fc53909
Joseph Corona <josed.hc96@gmail.com>
2018-11-05 01:58:29 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+
+  @name
+  @breed
+
+  def name=(dog_name)
+
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dog_name)
+
+    @name = dog_name
+  end
+
+  def breed
+    @name
+  end
+end
